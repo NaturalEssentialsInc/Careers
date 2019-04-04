ef97fded6bdd49a28009c866bb48da15981d67df
Ian Goodnight <Ian.Goodnight@bulkapothecary.com>
2019-04-04 11:27:43 -0400

    commented for reference

--- assets/app.js
+++ assets/app.js
@@ -1,39 +1,44 @@
+// document.ready keept scripts from running until the page has loaded.  Everything else on this page is wrapped inside this function.
 $(document).ready(function() {
-  const sheetID = "1InTwiH5M_JZ7MWmHHPD4TW1qDNAgN2ONOXCybSpzptg";
-  const url = "https://spreadsheets.google.com/feeds/list/" + sheetID + "/1/public/values?alt=json";
-  const scriptURL = "https://script.google.com/a/bulkapothecary.com/macros/s/AKfycbxzPHcIHjNMitxi04w3WQLXHaPyrCGNO1c4m9Qm/exec";
-  const form = document.forms["apply-now-careers"];
+  const sheetID = "1InTwiH5M_JZ7MWmHHPD4TW1qDNAgN2ONOXCybSpzptg"; // Reference to the sheet ID, abstracted so it can be changed easily.
+  const url = "https://spreadsheets.google.com/feeds/list/" + sheetID + "/1/public/values?alt=json"; // URL that exposes the JSON
+  // These next two are just for handling my form responses.  They aren't necessary for displaying data from a sheet
+  const scriptURL = "https://script.google.com/a/bulkapothecary.com/macros/s/AKfycbxzPHcIHjNMitxi04w3WQLXHaPyrCGNO1c4m9Qm/exec";  // This is for handling my form responses
+  const form = document.forms["apply-now-careers"]; // Reference my form
 
 	// AJAX call
 	$.ajax({
-    url : url,
-    type : 'GET',
-    dataType : 'jsonp',
+    url : url, // Declared above
+    type : 'GET', // GET method pulls data.
+    dataType : 'jsonp', // jsonp to avoid CORS issues
     success : function(res, status){
-        console.log('status : ' + status);
-        console.log('res: ' + res.feed);
-        var data = printResponse(res);
-        elementBuilder(data);
+        console.log('status : ' + status); // Log responses to the console
+        console.log('res: ' + res.feed); // the 'res' object contains the response fomr the AJAX call.
+        var data = printResponse(res); // printResponse is a custom function that takes entries from 'res' and stores them in a new object for easier access.
+        elementBuilder(data); // Custom function takes the data returned from printResponse and turns it into HTML on the page.
     },
-    error : function(res, status, error){
-        console.log('status : ' + status);
-        console.log(res);
-        console.log(error);
+    error : function(res, status, error){ // Error handling
+        console.log('status : ' + status); // Prints status header
+        console.log(res); // Logs response
+        console.log(error); // Logs Errors
     }
 	});
 
-	function printResponse(res) {
+	function printResponse(res) { // Custom function that takes the 'res' object and strips out the parts I want.
 
-		var entries = res.feed.entry;
-		var list = [];
+		var entries = res.feed.entry; // If you look at the JSON in your browser, you will see that everything on the sheet is in res.feed.entry
+		var list = []; // Empty array for storing my NEW object.
 
-		for (var i = 1; i < entries.length; i++) {
+		// Next we loop through the Rows to pull out what we need.
+		for (var i = 1; i < entries.length; i++) { // Looping through the 'entries' in 'res.feed.entry'
 
-			var e = {};
+			var e = {}; // Empty object as a placeholder for injecting it into my array
 
-			entries[i]["gsx$jobpostings"] ? e.id = entries[i]["gsx$jobpostings"].$t: e.id = "";
+			// ES6 syntax 'if [condition] ? then [execution] : else [execution]' the if and else are implied.
+			// Basically I am checking if these entries exist, and if they don't applying some default value.
+			entries[i]["gsx$jobpostings"] ? e.id = entries[i]["gsx$jobpostings"].$t: e.id = ""; // e.[variable] I am adding members to the empty object I initialized
 			entries[i]["gsx$_cokwr"] ? e.title = entries[i]["gsx$_cokwr"].$t: e.title = "TITLE UNAVAILABLE";
-			entries[i]["gsx$_cpzh4"] ? e.description = entries[i]["gsx$_cpzh4"].$t.replace(/(?:\r\n|\r|\n)/g, '<br>'): e.description = "";
+			entries[i]["gsx$_cpzh4"] ? e.description = entries[i]["gsx$_cpzh4"].$t.replace(/(?:\r\n|\r|\n)/g, '<br>'): e.description = ""; // Don't worry about the .replace().  It is just formatting REGEX to make the career entries look nice.
 			entries[i]["gsx$_cre1l"] ? e.duties = entries[i]["gsx$_cre1l"].$t.replace(/(?:\r\n|\r|\n)/g, '<br>'): e.duties = "";
 			entries[i]["gsx$_chk2m"] ? e.skills = entries[i]["gsx$_chk2m"].$t.replace(/(?:\r\n|\r|\n)/g, '<br>'): e.skills = "";
 			entries[i]["gsx$_ciyn3"] ? e.conditions = entries[i]["gsx$_ciyn3"].$t.replace(/(?:\r\n|\r|\n)/g, '<br>'): e.conditions = "";
@@ -43,14 +48,15 @@ $(document).ready(function() {
 
 			console.log(e.status);
 
-			list.push(e);
+			list.push(e); // Then I push my new object into my array for accessing later.  I declare 'e = {}' at the beginning of the loop, so each iteration empties the object and starts again.  The list is declared outside the loop, so it does not get overwritten.
 
 		}
 
-		return list;
+		return list; // Return my array of objects.  This is what is returned on line 17 and captured under the variable 'data'
 
 	}
 
+	// This is a custom function I created to deal with the markup I provided HR with to add bulleted lists to the page.  It doesn't really have any use outside of this context.
 	function listMaker(input) {
 
 	  var list = "";
@@ -68,15 +74,18 @@ $(document).ready(function() {
 		}
 	}
 
+	// This is where the object created in printResponse() and stored as the variable 'data' gets turned into HTML
 	function elementBuilder(data) {
 
-		var element = "";
-		var options = "";
+		var element = ""; // Empty variables for filling later
+		var options = ""; 
 
+		// I loop through my array of objects contained in data which is really just looping htrough the rows in my sheet.
+		// You could aslo access anything in data without looping (ie: data[position in array].member)
 		for (var i = 0; i < data.length; i++) {
-
+			// Then I create all my HTML based on my data and insert it into the page.  It's kind of roundabout, a templating engine like Handlebars would make this easier, but for somethign this simple, it was more straightforward to create the HTML myself.
       if (data[i].status === "ACTIVE") {
-
+      
 				var listing = "<div class=\"card\">" +
 											  "<div class=\"card-header\" id=\"heading" + i + "\">" +
 											    "<div class=\"row\">" +
@@ -115,22 +124,12 @@ $(document).ready(function() {
 			}
 		}
 
-		$("#jobs").append(element);
+		$("#jobs").append(element); // adds the HTML I've created to the element with the #jobs id.
 		$("#position").append(options);
 		$("#position").append("Any");
 	}
 
-  // form.addEventListener('submit', e => {
-  //   e.preventDefault();
-    
-  //   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  //     .then(response => console.log('Success!', response), 
-  //     	alert('Your application has been successfully submitted!'),
-  //     	$("#apply-now-careers").trigger("reset"),
-  //     	$("#apply-now-modal").modal("hide"))
-  //     .catch(error => alert('Error! Please re-submit your application!', error.message));
 
-  // }) 
   $("input[name='phone'], input[name='prior1-phone'], input[name='prior2-phone'], input[name='prior3-phone']").keypress(function (e) {
     if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
       return false;
